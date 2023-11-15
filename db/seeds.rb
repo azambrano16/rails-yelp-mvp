f602b99fd0e37a7126e601ddf52bfabb7355fc03
Andrea Zambrano <andreazambrano16@gmail.com>
2023-11-15 16:50:12 +0100

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+mcdonalds = { name: 'mcdonalds', address: 'madrid', phone_number: '678900654', category: 'fast food' }
+mediterranae = { name: 'mediterranae', address: 'madrid', phone_number: '677550664', category: 'mediterranean' }
+ginos = { name: 'ginos', address: 'madrid', phone_number: '644455544', category: 'italian'}
+telepizza = { name: 'telepizza', address: 'madrid', phone_number: '673330664', category: 'fastfood'}
+taggliatella = { name: 'taggliatella', address: 'madrid', phone_number: '672222664', category: 'italian' }
+
+
+[mcdonalds, mediterranae, ginos, telepizza, taggliatella].each do |restaurant|
+  restaurant = Restaurant.create!(restaurant)
+  puts "Created #{restaurant.name}"
+end
